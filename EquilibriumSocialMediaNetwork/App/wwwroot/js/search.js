6a2e2f3280cdb3cad025bd7854438c3cfb0a14e2
Vladimir Stoyanov <69723159+VladoDev03@users.noreply.github.com>
2022-02-03 22:37:46 +0200

    Added Search bar to Home page.

---
+++ EquilibriumSocialMediaNetwork/App/wwwroot/js/search.js
@@ -0,0 +1,27 @@
+﻿const userCardTemplate = document.querySelector("[data-user-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let users = []
+
+searchInput.addEventListener("input", (e) => {
+    const value = e.target.value.toLowerCase()
+    users.forEach(user => {
+        const isVisible = user.name.toLowerCase().includes(value)
+        user.element.classList.toggle("hide", !isVisible)
+    })
+    console.log(users)
+})
+
+fetch("https://localhost:44366/users")
+    .then(res => res.json())
+    .then(data => {
+        users = data.map(user => {
+            const card = userCardTemplate.content.cloneNode(true).children[0]
+            const header = card.querySelector("[data-header]")
+            header.textContent = user.UserName
+            userCardContainer.append(card)
+            console.log(user)
+            return { name: user.UserName, id: user.UserName, element: card}
+        })
+    })
