8ecf480eb7cfad90a9e23d2a945603998d27c7b7
Vladimir Stoyanov <69723159+VladoDev03@users.noreply.github.com>
2022-02-06 16:43:42 +0200

    Added FirstName, LastName and UserName to register.

--- EquilibriumSocialMediaNetwork/App/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ EquilibriumSocialMediaNetwork/App/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -80,9 +80,11 @@ namespace App.Areas.Identity.Pages.Account
         
             if (ModelState.IsValid)
             {
+                var user = await _userManager.FindByNameAsync(Input.Email) ?? await _userManager.FindByEmailAsync(Input.Email);
+
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(user.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
--- EquilibriumSocialMediaNetwork/App/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ EquilibriumSocialMediaNetwork/App/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -47,6 +47,18 @@ namespace App.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [Display(Name = "User name")]
+            public string UserName { get; set; }
+
+            [Required]
+            [Display(Name = "First name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last name")]
+            public string LastName { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -75,7 +87,13 @@ namespace App.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new User { UserName = Input.Email, Email = Input.Email };
+                var user = new User
+                {
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    UserName = Input.UserName,
+                    Email = Input.Email
+                };
 
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
