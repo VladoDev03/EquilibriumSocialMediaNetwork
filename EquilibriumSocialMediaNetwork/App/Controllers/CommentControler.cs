4185629a5a5ea389ec9705c0fb11b561caaf7a47
bibiatanasova <78483307+bibiatanasova@users.noreply.github.com>
2022-02-27 18:41:52 +0200

    comment controller

--- EquilibriumSocialMediaNetwork/App/Controllers/CommentControler.cs
+++ EquilibriumSocialMediaNetwork/App/Controllers/CommentControler.cs
@@ -1,33 +1,67 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Data.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
 namespace App.Controllers
 {
-    public class CommentControler
+    public class CommentControler : Controller
     {
-        public CommentControler()
+        private IUserServices userServices;
+        private UserManager<User> userManager;
+
+        public CommentControler(IUserServices userServices, UserManager<User> userManager)
         {
+            this.userServices = userServices;
+            this.userManager = userManager;
+        }
 
-
-            /*private void AddComment()
+        [HttpGet]
+        private IActionResult AddComment()
+        {
+            if (!this.User.Identity.IsAuthenticated)
             {
-                string content = Console.ReadLine();
-
-                Comment comment = new Comment();
-                this.userServices.AddComment(comment);
+                return Redirect("Index");
             }
 
-            private void UpdateComment()
-            {
-                string idComment = Console.ReadLine();
+            return View();
+        }
 
-                Comment comment = new Comment();
-                this.userServices.UpdateComment(comment);
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> AddComment(Comment comment)
+        {
+            User user = await this.userManager.GetUserAsync(this.User);
+            this.userServices.AddComment(comment);
+            return RedirectToAction("Add Comment");
+        }
+
+
+        [HttpPost]
+        public IActionResult UpdateComment(Comment updateComment)
+        {
+            this.userServices.UpdateComment(updateComment);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ActionName("Delete comment")]
+        public IActionResult DeleteComment(string id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
             }
-            private void DeleteComment()
-            {
-                string idComment = Console.ReadLine();
 
-                this.userServices.DeleteComment(idComment);
-            }*/
+            this.userServices.DeleteComment(id);
+
+            return RedirectToAction("Index");
+
+
+
         }
     }
 }
