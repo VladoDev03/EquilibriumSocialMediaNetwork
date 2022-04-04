b256488cc57f66047e1a5f940be38ce5314c325a
Vladimir Stoyanov <69723159+VladoDev03@users.noreply.github.com>
2022-04-04 21:43:59 +0300

    Chat scrolls to bottom when message is sent.

--- EquilibriumSocialMediaNetwork/App/wwwroot/js/chat.js
+++ EquilibriumSocialMediaNetwork/App/wwwroot/js/chat.js
@@ -1,5 +1,4 @@
 ﻿const config = {
-    //idOne: document.getElementById('userOne').innerText,
     idTwo: document.getElementById('userTwo').innerText,
     conversationId: document.getElementById('conversation-to-find').innerText
 };
@@ -8,49 +7,60 @@ const attemptChatLogin = () => {
     const connection = new signalR.HubConnectionBuilder()
         .withUrl("https://localhost:44366/chat")
         .configureLogging(signalR.LogLevel.Information)
-        .build();
+        .build()
 
     async function start() {
         try {
-            await connection.start();
-            console.log("SignalR Connected.");
+            await connection.start()
+            console.log("SignalR Connected.")
         } catch (err) {
-            console.log(err);
-            setTimeout(start, 5000);
+            console.log(err)
+            setTimeout(start, 5000)
         }
     }
 
     connection.onclose(async () => {
-        await start();
+        await start()
     });
 
     connection.on("ReceiveMessage", (message) => {
-        const h1 = document.createElement("h1");
-        h1.textContent = `${message}`;
-        document.getElementById('messages').appendChild(h1);
+        const h1 = document.createElement("h1")
+        h1.textContent = `${message}`
+        document.getElementById('messages').appendChild(h1)
+        window.scroll(0, document.documentElement.scrollHeight)
     });
 
-    document.getElementById('send-button').addEventListener('click', async () => {
-        const message = document.getElementById('message').value;
+    async function sendMessage() {
+        const message = document.getElementById('message').value
 
         if (message) {
             await connection.invoke("SendMessage", config.idTwo, message);
-            document.getElementById('message').value = "";
+            document.getElementById('message').value = ""
         }
+    }
+
+    document.getElementById('send-button').addEventListener('click', async () => {
+        await sendMessage()
     });
 
-    start();
+    document.getElementById('message').addEventListener('keyup', async e => {
+        if (e.key === 'Enter' || e.keyCode === 13) {
+            await sendMessage()
+        }
+    })
+
+    start()
 }
 
 const displayChatWrapper = () => {
-    document.getElementById('chat-wrapper').style.display = 'block';
+    document.getElementById('chat-wrapper').style.display = 'block'
 
     getConversation(config.conversationId)
         .then(conversation => {
             conversation.messages.forEach(message => {
-                const h1 = document.createElement("h1");
-                h1.textContent = `[${message.usernameOne}] ${message.content}`;
-                document.getElementById('messages').appendChild(h1);
+                const h1 = document.createElement("h1")
+                h1.textContent = `[${message.usernameOne}] ${message.content}`
+                document.getElementById('messages').appendChild(h1)
             })
         })
 }
