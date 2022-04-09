c72c616f5370c1cc0032dee72958b751fa7d0cc1
Vladimir Stoyanov <69723159+VladoDev03@users.noreply.github.com>
2022-04-09 16:03:13 +0300

    Added Ani's views to backend.

---
+++ EquilibriumSocialMediaNetwork/App/wwwroot/js/preview.js
@@ -0,0 +1,12 @@
+﻿var textcontent = document.getElementById('textcontent');
+
+textcontent.onkeyup = textcontent.onkeypress = function () {
+    document.getElementById('showtext').innerHTML = this.value;
+}
+
+imgInp.onchange = evt => {
+    const [file] = imgInp.files
+    if (file) {
+        show_Image.src = URL.createObjectURL(file)
+    }
+}
